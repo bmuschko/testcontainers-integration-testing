06790a3e95cae525673717850763cd5d301fd4c1
Benjamin Muschko <benjamin.muschko@gmail.com>
2021-10-17 17:15:56 -0600

    Reorganize code

--- exercises/03-docker-compose/solution/multi-container/src/main/java/com/bmuschko/testcontainers/repository/warehouse/index/ProductIndexRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bmuschko.testcontainers.repository.warehouse.index;
-
-import com.bmuschko.testcontainers.model.warehouse.Product;
-
-public interface ProductIndexRepository {
-    void insertProduct(Product product);
-}
--- exercises/03-docker-compose/solution/multi-container/src/main/java/com/bmuschko/testcontainers/service/WarehouseService.java
+++
@@ -1,7 +0,0 @@
-package com.bmuschko.testcontainers.service;
-
-import com.bmuschko.testcontainers.model.warehouse.Product;
-
-public interface WarehouseService {
-    void addProduct(Product product);
-}
